3f4df01689660c6c6b2f2d761d496321598b77c5
Will16 <williammcduff@Williams-MacBook-Pro-2.local>
2015-03-26 20:27:55 -0400

    push
    push

--- Rails/Wingman/BrowseTableViewController.swift
+++ Rails/Wingman/BrowseTableViewController.swift
@@ -263,7 +263,7 @@ class BrowseTableViewController: UITableViewController,  didGetEventsProtocol {
                 println(url)
                 let data = NSData(contentsOfURL: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check
                 
-                cell.userImage.image =  UIImage(data: data!)
+               //cell.userImage.image =  UIImage(data: data!)
                 
                
             }
